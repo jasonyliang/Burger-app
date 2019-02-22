f65173be564456aa54aa62f6d925f8207ce84a64
Jason Liang <jjjasonliang@gmail.com>
2019-02-22 22:44:30 +0530

    improve performance by adding shouldComponentUpdate conditions

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
-  return (
-    <>
-      <h3>Your Order:</h3>
-      <p>A wonderful burger with the following:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: ${props.price.toFixed(2)}</strong>
-      </p>
-      <p>Ready to Checkout?</p>
-      <Button btnType="Danger" clicked={props.canceled}>
-        Cancel
-      </Button>
-      <Button btnType="Success" clicked={props.continued}>
-        Continue
-      </Button>
-    </>
-  );
-};
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log("[Order Summary] will update");
+  }
 
-export default orderSummary;
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
+    return (
+      <>
+        <h3>Your Order:</h3>
+        <p>A wonderful burger with the following:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Ready to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.canceled}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.continued}>
+          Continue
+        </Button>
+      </>
+    );
+  }
+}
+
+export default OrderSummary;

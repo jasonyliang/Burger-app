da7478f71322d4dbb0537c3d0e2df2963d0eaa3a
Jason Liang <jjjasonliang@gmail.com>
2019-03-10 11:18:37 +0530

    post dummy data

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICE = {
   salad: 0.5,
@@ -74,7 +75,25 @@ class BurgerBuilder extends Component {
   };
 
   checkoutContinueHandler = () => {
-    alert("You continued!");
+    // alert("You continued!");
+    const order = {
+      incredient: this.state.ingredient,
+      price: this.state.totalPrice,
+      customer: {
+        name: "XDD",
+        address: {
+          street: "test street",
+          zipCode: "42134",
+          country: "South Korea"
+        },
+        email: "Test@email.com"
+      },
+      delivery: "Fastest"
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
 
   render() {

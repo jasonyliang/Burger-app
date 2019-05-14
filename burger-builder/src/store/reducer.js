9279236a665d62a3b54d23903d24ebba08a1455f
Jason Liang <jjjasonliang@gmail.com>
2019-05-14 18:14:59 +0800

    reducer and action types

---
+++ burger-builder/src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./action";
+
+const initialState = {
+  ingredient: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;

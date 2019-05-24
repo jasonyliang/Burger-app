183bbfd49c7e89c9e23239565ad56c6c2d828e63
Jason Liang <jjjasonliang@gmail.com>
2019-05-24 08:51:17 +0800

    combining reducers

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -168,9 +168,9 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    tprice: state.totalPrice,
-    error: state.error
+    ings: state.burgerBuilder.ingredients,
+    tprice: state.burgerBuilder.totalPrice,
+    error: state.burgerBuilder.error
   };
 };
 

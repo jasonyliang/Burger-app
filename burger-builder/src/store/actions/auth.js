2d09d50a52afb7c1c55043171c2434018489bc7a
Jason Liang <jjjasonliang@gmail.com>
2019-06-03 18:05:12 +0800

    sign up feature

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -1,4 +1,6 @@
 import * as actionTypes from "./actionTypes";
+// not our default url
+import axios from "axios";
 
 export const authStart = () => {
   return {
@@ -23,5 +25,23 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA1-Sp1x0L-HNv30K_bmw_SecpIb2tEiVw",
+        authData
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
